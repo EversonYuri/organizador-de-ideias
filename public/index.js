08f73cfe437dfa48b5feab9ce5e7fa653c3bd090
Andrei Lucas Gonçalves <andrei.lucas.goncalves@hotmail.com>
2020-01-14 20:37:40 -0300

    Adicionado comentários ao index.js

--- public/index.js
+++ public/index.js
@@ -1,24 +1,25 @@
+// Pegando os elementos HTML
 const spanValor = document.getElementById('valor')
 const botaoMudar = document.getElementById('botao-mudar')
 
-
+// Abrindo o database
 var database = firebase.database()
 
+// Pegando referencias do Database
 const algumacoisa_ref = database.ref('/alguma-coisa')
 
-algumacoisa_ref.on('value', mudou)
 
-function mudou(snapshot) {
-    spanValor.innerText = snapshot.val()
+// Toda vez que faz alguma modificação ou inicia
+// Ele chama a função anonima
+algumacoisa_ref.on('value', function(snapshot) {
+    spanValor.innerText = snapshot.val() // Aqui muda o elemento HTML
     console.log(snapshot.val())
-}
+})
 
 
-
-
-
+// Quando clica no botão mudar
 botaoMudar.onclick = function() {
     const num = Math.random()
-    algumacoisa_ref.set(num)
+    algumacoisa_ref.set(num) // Aqui que modifica o banco
     alert('Mudou! ' + num)
 }
